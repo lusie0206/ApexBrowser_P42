4db873f66939deadb91663e0677533fb73ea6119
lusie0206 <lusie0206@gmail.com>
2026-05-29 17:51:19 +0300

    Added test button into the Form1

--- ApexBrowser/Form1.Designer.cs
+++ ApexBrowser/Form1.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonTest = new Button();
+            SuspendLayout();
+            // 
+            // buttonTest
+            // 
+            buttonTest.Location = new Point(694, 12);
+            buttonTest.Name = "buttonTest";
+            buttonTest.Size = new Size(94, 29);
+            buttonTest.TabIndex = 0;
+            buttonTest.Text = "Test";
+            buttonTest.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonTest);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button buttonTest;
     }
 }
